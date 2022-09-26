5f0115801b3839ba83b91841bbe408f0997b3132
HOANGANH\Lenovo <hoanganh5111999@gmail.com>
2022-09-26 15:20:03 +0700

    Pointer receiver

--- method.go
+++ method.go
@@ -8,6 +8,9 @@ type Student struct {
 
 //Define method
 //func (t Type) method(params) return { //code}
+func (s Student) getNameI() string {
+	return s.name
+}
 
 //(t Type) => Receiver
 //1.Value Receiver:Không làm thay đổi giá trị field trong struct
@@ -15,19 +18,25 @@ func (s Student) changeName() {
 	s.name = "Mary"
 }
 
-//2.Poiter Receiver
+//2.Pointer Receiver:Thay đổi field gốc trong struct
+func (s *Student) changeName2() {
+	fmt.Printf("p1 = %p", s)
 
-func (s Student) getNameI() string {
-	return s.name
+	s.name = "Mary"
 }
 
 func main() {
-	student := Student{"kai"}
+	student := &Student{"kai"}
 
-	name := student.getNameI()
+	// name := student.getNameI()
 
-	fmt.Println(name)
+	// fmt.Println(name)
 
-	student.changeName()
+	// student.changeName()
+	// fmt.Println(student.name)
+
+	fmt.Printf("p1 = %p", student)
+	fmt.Println()
+	student.changeName2()
 	fmt.Println(student.name)
 }
